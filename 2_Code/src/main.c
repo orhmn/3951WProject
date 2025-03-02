7854b89558934f0d2e1253488bd424021c265c09
Nicholas Erickson <131315614+eric4767@users.noreply.github.com>
2025-03-01 18:45:41 -0600

    Update main.c
    rough draft

--- 2_Code/src/main.c
+++ 2_Code/src/main.c
@@ -1,18 +1,39 @@
-/* 
- * File:   main.c
- * Author: orahm
- *
- * Created on March 1, 2025, 4:46 PM
- */
-
-#include <stdio.h>
-#include <stdlib.h>
-
 /*
- * 
+ * File:   main.c
+ * Author: sethrosenberg
+ *
+ * Created on April 12, 2024, 11:29 AM
  */
-int main(int argc, char** argv) {
 
-    return (EXIT_SUCCESS);
+#include "xc.h"
+#include "delay.h"
+#include "SPI.h"
+#include "data_buffer.h"
+
+// CW1: FLASH CONFIGURATION WORD 1 (see PIC24 Family Reference Manual 24.1)
+#pragma config ICS = PGx1          // Comm Channel Select (Emulator EMUC1/EMUD1 pins are shared with PGC1/PGD1)
+#pragma config FWDTEN = OFF        // Watchdog Timer Enable (Watchdog Timer is disabled)
+#pragma config GWRP = OFF          // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF           // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF        // JTAG Port Enable (JTAG port is disabled)
+
+
+// CW2: FLASH CONFIGURATION WORD 2 (see PIC24 Family Reference Manual 24.1)
+#pragma config I2C1SEL = PRI       // I2C1 Pin Location Select (Use default SCL1/SDA1 pins)
+#pragma config IOL1WAY = OFF       // IOLOCK Protection (IOLOCK may be changed via unlocking seq)
+#pragma config OSCIOFNC = ON       // Primary Oscillator I/O Function (CLKO/RC15 functions as I/O pin)
+#pragma config FCKSM = CSECME      // Clock Switching and Monitor (Clock switching is enabled, 
+                                       // Fail-Safe Clock Monitor is enabled)
+#pragma config FNOSC = FRCPLL      // Oscillator Select (Fast RC Oscillator with PLL module (FRCPLL))
+
+volatile uint16_t data = 0;
+
+
+void __attribute__((interrupt, auto_psv)) _CNInterrupt(void) {
+    IFS1bits.CNIF = 0;  // Clear interrupt flag
+    uint16_t data = ADC_read();  // Read data
+    // Process data (e.g., store in a buffer or variable)
 }
+
+
 
