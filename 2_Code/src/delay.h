1f29c29fbbdb6c02742a2a26c097407a9bbb1364
Nicholas Erickson <131315614+eric4767@users.noreply.github.com>
2025-03-01 18:49:49 -0600

    Create delay.h
    rough draft

---
+++ 2_Code/src/delay.h
@@ -0,0 +1,19 @@
+
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+
+#include <xc.h>
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+    
+    void wait_1ms();
+    void wait_100us();
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
